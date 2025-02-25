c8a45a63237224db38de8ecc3d5428666e8cbda7
moonjeli <chadheer@yahoo.com>
2025-02-25 13:36:30 -0500

    adding to plot_odors script

--- Chad/UpWin_2025.m
+++ Chad/UpWin_2025.m
@@ -1,3 +1,25 @@
+%% 02132025
+% ImagingData/02132025/fly001
+% input params, 5 fps, 226 frames, 15 roi size
+ 
+folder = 'UPWIN/02132025/fly001/ACV_left';
+
+data.fly02132025_001.ACV_left = extract_fictrac(folder);
+% 
+folder = 'UPWIN/02132025/fly001/pre';
+
+data.fly02132025_001.pre = extract_fictrac(folder);
+% 
+folder = 'UPWIN/02132025/fly001/post';
+
+data.fly02132025_001.post = extract_fictrac(folder);
+
+data.fly02132025_001.post.paired_odor = "oct";
+data.fly02132025_001.pre.paired_odor = "oct";
+
+folder = 'UPWIN/02132025/fly001';
+data.fly02132025_001 = extract_fluorescence(data.fly02132025_001, folder)
+
 %% 02202025
 % ImagingData/02202025/fly001
 % input params, 5 fps, 226 frames, 15 roi size
@@ -19,6 +41,8 @@ data.fly02202025_001.pre.paired_odor = "mch";
 
 folder = 'UPWIN/02202025/fly001';
 data.fly02202025_001 = extract_fluorescence(data.fly02202025_001, folder)
+
+
 
 %%
 F_and_beh(data,'movspd');
